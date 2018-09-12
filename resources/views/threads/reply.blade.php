7f25ff643f64158d94332dcfc105d95d2dba711e
Raül San Clemente <raulsancal@gmail.com>
2018-09-12 13:25:12 +0200

    created Thread unit test

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,7 +1,12 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: user
- * Date: 12/09/2018
- * Time: 13:15
- */
+<div class="card">
+    <div class="card-header">
+        {{ $reply->owner->name }} said {{ $reply->created_at->diffForHumans() }}
+    </div>
+    <div class="card-body">
+        <article>
+            <div class="body">{{ $reply->body }}</div>
+        </article>
+
+        <hr>
+    </div>
+</div>
--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -21,18 +21,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 @foreach($thread->replies as $reply)
-                    <div class="card">
-                        <div class="card-header">
-                            {{ $reply->owner->name }} said {{ $reply->created_at->diffForHumans() }}
-                        </div>
-                        <div class="card-body">
-                            <article>
-                                <div class="body">{{ $reply->body }}</div>
-                            </article>
-
-                            <hr>
-                        </div>
-                    </div>
+                    @include('threads.reply')
                 @endforeach
             </div>
         </div>
